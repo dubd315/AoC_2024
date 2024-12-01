bd8855b72ec0bfb5488f012f7d3ce17ef27716c8
dubd315 <dubd315@gmail.com>
2024-12-01 18:01:53 -0500

    minor adjustments

--- Day1/Day1Part2.java
+++ Day1/Day1Part2.java
@@ -10,18 +10,18 @@ public class Day1Part2 {
     public static void main(String[] args) throws IOException{
         String filepath = "AoC_2024/Day1/inputs/Day1Input.txt";
         int similarityScore = 0;
-        Boolean bool = true;
+        boolean bool = true;
         ArrayList<Integer> leftList = new ArrayList<>();
         ArrayList<Integer> rightList = new ArrayList<>();
         try {
             BufferedReader reader = new BufferedReader(new FileReader(filepath));
-            while (bool == true){
+            while (bool){
                 String currentLine = reader.readLine();
                 if(currentLine == null){
                     bool = false;
                     reader.close();
                 } else {
-                    String[] vals = currentLine.split("   ");
+                    String[] vals = currentLine.split(" {3}");
                     int firstNum = Integer.parseInt(vals[0]);
                     leftList.add(firstNum);
                     int secondNum = Integer.parseInt(vals[1]);
